5174ef748bae4813c7119e1bb500c9eef0aef868
glk12 <glopesaraujo6@gmail.com>
2026-02-27 04:05:19 -0300

    minor changes

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: bash release.sh
+web: cd djangoseed && /app/.venv/bin/gunicorn djangoseed.wsgi:application --bind 0.0.0.0:$PORT --log-file -
+
--- release.sh
+++ release.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-
-/app/.venv/bin/python djangoseed/manage.py migrate --noinput && \
-/app/.venv/bin/python djangoseed/manage.py collectstatic --noinput && \
-/app/.venv/bin/gunicorn djangoseed.wsgi:application --bind 0.0.0.0:$PORT --log-file -
+web: cd djangoseed && /app/.venv/bin/python manage.py migrate --noinput && /app/.venv/bin/python manage.py collectstatic --noinput && /app/.venv/bin/gunicorn djangoseed.wsgi:application --bind 0.0.0.0:$PORT --log-file -
